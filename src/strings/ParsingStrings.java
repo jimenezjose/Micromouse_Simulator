8eb5bf3284f1c66e8ae49f645ef8fd6a714c2eee
Jose Jimenez-Olivas <josejimenezjr18@gmail.com>
2019-02-23 20:19:28 -0800

    maze generation flags

--- src/strings/ParsingStrings.java
+++ src/strings/ParsingStrings.java
@@ -1,6 +1,6 @@
 public class ParsingStrings {
 
-    public static final String USAGE = "Usage: java MazeGUI [-help | -h] [-dimension number | -d number] [-dijkstra] [-dfs]";
+    public static final String USAGE = "Usage: java MazeGUI [-help] [-dimension number ] [-maxcycles number] [-dijkstra] [-dfs]";
     public static final String HELP_FLAG_1 = "-h";
     public static final String HELP_FLAG_2 = "-help";
     public static final String DIM_FLAG_1 = "-d";
@@ -9,7 +9,10 @@ public class ParsingStrings {
     public static final String CYCLES_FLAG_2 = "-mc";
     public static final String DIJKSTRA_FLAG = "-dijkstra";
     public static final String DFS_FLAG = "-dfs";
-    public static final String HELP_MSG = "TODO";
+    public static final String HELP_MSG = "\n-help | -h\t\tHelp message\n" + 
+      "-dimension | -d\t\tDimension size of maze generated\n-maxcycles | -mc\t" +
+      "Maximum number of cycles in maze\n-dijkstra\t\tPerform Dijkstra's " +
+      "Algorithm path traversal\n-dfs\t\t\tDepth First Search solution path traversal\n";
 
     public static final String[] FLAGS = { 
       HELP_FLAG_1, HELP_FLAG_2, DIM_FLAG_1, DIM_FLAG_2, CYCLES_FLAG_1, 
